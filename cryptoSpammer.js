dd78055228108f7f109df4acef1930f4d27824d8
Oleksandr Khotemskyi <xotabu4@gmail.com>
2017-07-31 10:53:35 +0300

    small fix
    small fix

--- cryptoSpammer.js
+++ cryptoSpammer.js
@@ -7,22 +7,17 @@ module.exports.addCryptoCurrencySpammer = function (bot) {
     bot.onText(/\/rates/, (msg, match) => {
         Promise.all([request('https://cex.io/api/last_price/BTC/USD'),
         request('https://cex.io/api/last_price/ETH/USD')])
-            .then(jsones => {
+            .then(responses => {
                 // Parsing all responces to JSON
-                jsones = jsones.map(json => JSON.parse(json))
+                jsones = responses.map(json => JSON.parse(json))
 
                 if (jsones[0].error || jsones[1].error) {
                     throw Error(`${jsones[0].error} : ${jsones[1].error}`)
                 }
                 const btcPrice = jsones[0].lprice
                 const ethPrice = jsones[1].lprice
-
-                const formattedRates =
-                    ```
-                CEX.IO rates:
-                BTC/USD - ${btcPrice}
-                ETH/USD - ${ethPrice}
-                ```
+                console.log('prices', btcPrice, ethPrice)
+                let formattedRates = `CEX.IO rates:\r\nBTC/USD - $${btcPrice}\r\nETH/USD - $${ethPrice}`;
 
                 bot.sendMessage(msg.chat.id, formattedRates)
             })
