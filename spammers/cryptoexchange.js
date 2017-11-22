d4018991890080ddb087b0a5f8ca5869f0190c75
Oleksandr Khotemskyi <xotabu4@gmail.com>
2017-11-22 15:47:46 +0200

    Rewritten crypto, testing
    Rewritten crypto, testing

--- spammers/cryptoexchange.js
+++ spammers/cryptoexchange.js
@@ -1,29 +1,37 @@
-let request = require('request-promise');
+const request = require('request-promise');
 /**
- * on /rates in chat - doing 2 requests, for ETH and BTC
+ * On /rates in chat - doing 2 requests, for ETH and BTC
  * waiting for promises and then formatting prices, and send message to chat
  */
+
+// Matches /echo [whatever]
+// bot.onText(/\/echo (.+)/, function onEchoText(msg, match) {
+//     const resp = match[1];
+//     bot.sendMessage(msg.chat.id, resp);
+//   }); 
 module.exports.addCryptoCurrencySpammer = function (bot) {
-    bot.onText(/\/rates/, (msg, match) => {
-        Promise.all([request('https://cex.io/api/last_price/BTC/USD'),
-        request('https://cex.io/api/last_price/ETH/USD')])
-            .then(responses => {
-                // Parsing all responces to JSON
-                jsones = responses.map(json => JSON.parse(json))
-
-                if (jsones[0].error || jsones[1].error) {
-                    throw Error(`${jsones[0].error} : ${jsones[1].error}`)
-                }
-                const btcPrice = jsones[0].lprice
-                const ethPrice = jsones[1].lprice
-                console.log('prices', btcPrice, ethPrice)
-                let formattedRates = `CEX.IO rates:\r\nBTC/USD - $${btcPrice}\r\nETH/USD - $${ethPrice}`;
-
-                bot.sendMessage(msg.chat.id, formattedRates)
-            })
-            .catch(function (err) {
-                console.error(err)
-                bot.sendMessage(msg.chat.id, `Что-то наебнулось, вот ошибка - ${err}`)
-            })
+    bot.onText(/\/rates (.+) (.+)/, (msg, match) => {
+        if (match[1] && match[2]) {
+            requestExchangeRate(match[1], match[2])
+        } else {
+            requestExchangeRate('BTC', 'USD')
+        }
     })
+}
+
+function requestExchangeRate(from, to) {
+    request(`https://cex.io/api/last_price/${from}/${to}`)
+        .then(response => {
+            json = JSON.parse(response)
+            if (json.error) {
+                throw Error(`${jsone.error}`)
+            }
+            const price = json.lprice
+            let formattedRates = `CEX.IO rates:\r\n${from}/${to} - ${price}`;
+            bot.sendMessage(msg.chat.id, formattedRates)
+        })
+        .catch(function (err) {
+            console.error(err)
+            bot.sendMessage(msg.chat.id, `Что-то упало, вот ошибка - ${err}`)
+        })
 }
