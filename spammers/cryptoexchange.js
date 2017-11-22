4e55b269feed79d0e3e8ada0257f1fc073fcd305
Oleksandr Khotemskyi <xotabu4@gmail.com>
2017-11-22 15:50:54 +0200

    fix
    fix

--- spammers/cryptoexchange.js
+++ spammers/cryptoexchange.js
@@ -10,6 +10,23 @@ const request = require('request-promise');
 //     bot.sendMessage(msg.chat.id, resp);
 //   }); 
 module.exports.addCryptoCurrencySpammer = function (bot) {
+    function requestExchangeRate(from, to) {
+        request(`https://cex.io/api/last_price/${from}/${to}`)
+            .then(response => {
+                json = JSON.parse(response)
+                if (json.error) {
+                    throw Error(`${jsone.error}`)
+                }
+                const price = json.lprice
+                let formattedRates = `CEX.IO rates:\r\n${from}/${to} - ${price}`;
+                bot.sendMessage(msg.chat.id, formattedRates)
+            })
+            .catch(function (err) {
+                console.error(err)
+                bot.sendMessage(msg.chat.id, `Что-то упало, вот ошибка - ${err}`)
+            })
+    }
+
     bot.onText(/\/rates (.+) (.+)/, (msg, match) => {
         if (match[1] && match[2]) {
             requestExchangeRate(match[1], match[2])
@@ -19,19 +36,3 @@ module.exports.addCryptoCurrencySpammer = function (bot) {
     })
 }
 
-function requestExchangeRate(from, to) {
-    request(`https://cex.io/api/last_price/${from}/${to}`)
-        .then(response => {
-            json = JSON.parse(response)
-            if (json.error) {
-                throw Error(`${jsone.error}`)
-            }
-            const price = json.lprice
-            let formattedRates = `CEX.IO rates:\r\n${from}/${to} - ${price}`;
-            bot.sendMessage(msg.chat.id, formattedRates)
-        })
-        .catch(function (err) {
-            console.error(err)
-            bot.sendMessage(msg.chat.id, `Что-то упало, вот ошибка - ${err}`)
-        })
-}
