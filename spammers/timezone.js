0d1b06f51d47c62c712d72ff51b35bab7a34494b
Oleksandr Khotemskyi <xotabu4@gmail.com>
2017-12-02 12:48:13 +0200

    simplified
    simplified

--- spammers/timezone.js
+++ spammers/timezone.js
@@ -1,29 +1,13 @@
 let moment = require('moment-timezone')
 
-let zonesMapping = {
-    'bali': "Asia/Makassar",
-    'рай': "Asia/Makassar",
-    'kiev': 'Europe/Kiev',
-    'kyiv': 'Europe/Kiev',
-    'киев': 'Europe/Kiev'
-}
-
 module.exports.addTimezoneSpammer = function (bot) {
-    bot.onText(/\/time (.+)?/, async (msg, match) => {
-        let city = match[1].toLowerCase()
-        let mapped = zonesMapping[city]
-        if(!mapped) {
-            for (let zone of moment.tz.names()) {
-                if(zone.toLowerCase().includes(city)) {
-                    mapped = zone
-                }
-            }
-        }
-        try {
-            let currenttime = moment.tz(moment(), mapped).format('MMMM D H:mm:ss')
-            bot.sendMessage(msg.chat.id, `В ${city} (${mapped}) : ${currenttime}`)
-        } catch (error) {
-            bot.sendMessage(msg.chat.id, `ERROR: ${JSON.stringify(error)}`)
-        }        
+    bot.onText(/\/time?/, async (msg, match) => {
+        let kievTime = moment.tz(moment(), 'Europe/Kiev').format('MMMM D || H:mm:ss')
+        let baliTime = moment.tz(moment(), 'Asia/Makassar').format('MMMM D || H:mm:ss')
+        bot.sendMessage(msg.chat.id, 
+`
+В Киеве : ${kievTime}
+На Бали: ${baliTime}
+`)    
     })
 }
