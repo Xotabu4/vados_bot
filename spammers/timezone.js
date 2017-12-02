375696eb183212c6156faf653f79ada42902aec5
Oleksandr Khotemskyi <xotabu4@gmail.com>
2017-12-02 12:49:46 +0200

    format fix
    format fix

--- spammers/timezone.js
+++ spammers/timezone.js
@@ -2,8 +2,8 @@ let moment = require('moment-timezone')
 
 module.exports.addTimezoneSpammer = function (bot) {
     bot.onText(/\/time?/, async (msg, match) => {
-        let kievTime = moment.tz(moment(), 'Europe/Kiev').format('MMMM D || H:mm:ss')
-        let baliTime = moment.tz(moment(), 'Asia/Makassar').format('MMMM D || H:mm:ss')
+        let kievTime = moment.tz(moment(), 'Europe/Kiev').format('MMMM D  H:mm:ss')
+        let baliTime = moment.tz(moment(), 'Asia/Makassar').format('MMMM D  H:mm:ss')
         bot.sendMessage(msg.chat.id, 
 `
 В Киеве : ${kievTime}
