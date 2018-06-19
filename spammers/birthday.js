d5a0a9558144838d82330ddf4e267d6d02e3382f
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-06-19 22:19:31 +0300

    fix2

--- spammers/birthday.js
+++ spammers/birthday.js
@@ -1,17 +1,16 @@
 module.exports.addBirthdaySpammer = function (bot) {
 
-    bot.onText(/бот, когда у ковы днюха?/, async (msg, match) => {
+    bot.onText(/бот, а ты кову поздравил?/, async (msg, match) => {
 
         let delayedSay = (message, ms) => {
             setTimeout(() => {
                 bot.sendMessage(msg.chat.id, message)
             }, ms)
         }
-
-        delayedSay('19 июля', 1000)
-        delayedSay('Это ж сегодня', 2000)
-        delayedSay('==================', 5000)
-        delayedSay('ОГО поздравляю счастья здоровля', 6000)
+        delayedSay('ковелова', 1000)
+        delayedSay('счастья здоровля', 2000)
+        delayedSay('{{ВСТАВЬТЕ СООБЩЕНИЕ ЗДЕСЬ}}', 5000)
+        delayedSay('давайте полюбуемся еще раз', 6000)
         let stickers = [
             'CAADAgADZwAD9-dEB8fm-zJm_0EbAg', // ОООО
             'CAADAgADaQAD9-dEB_YJsckeeORpAg', // лукавая
@@ -20,7 +19,7 @@ module.exports.addBirthdaySpammer = function (bot) {
             'CAADAgADQQEAAvfnRAeFc-WhnCbivgI' // Недовольная
         ]
         stickers.forEach(sticker => delayedSay(sticker, 10000))
-        delayedSay('==================', 11000)
+        delayedSay('ах, наша красоточка', 11000)
     })
 }
 
