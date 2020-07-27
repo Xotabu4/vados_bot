850db73e61e594a73e277e4d8e7756db9757fa50
Oleksandr Khotemskyi <xotabu4@gmail.com>
2020-07-27 12:52:06 +0300

    Update bajaj_remover.js

--- spammers/bajaj_remover.js
+++ spammers/bajaj_remover.js
@@ -1,7 +1,9 @@
 
 module.exports.addBajajRemover = function (bot) {
+    const pashaID = 238686566
+
     bot.onText(/убей байай|убей bajaj/gi, (msg, match) => {
-        if (msg.reply_to_message) {
+        if (msg.reply_to_message && msg.photo && msg.from.id === pashaID) {
             Promise.all([
                 bot.deleteMessage(msg.chat.id, msg.message_id),
                 bot.deleteMessage(msg.chat.id, msg.reply_to_message.message_id)
@@ -9,7 +11,7 @@ module.exports.addBajajRemover = function (bot) {
                 bot.sendMessage(msg.chat.id, err.message)
             })
         } else {
-            bot.sendMessage(msg.chat.id, 'Чтобы уничтожить bajaj - сделай реплай на сообщение с словами - убей байай|убей bajaj ')
+            bot.sendMessage(msg.chat.id, 'Чтобы уничтожить bajaj - сделай реплай на пашино сообщение с фоткой, и сообщением - убей байай|убей bajaj ')
         }
     })
 }
